394f4be368ad1d839afad8a8111ae2c52972f83c
MEMOPERF <contact@checkupauto.fr>
2026-09-10 01:52:05 +0200

    chore(pl-dc): preserve merged reachability import

--- ProofLab.lean
+++ ProofLab.lean
@@ -4,4 +4,5 @@ import ProofLab.Descriptive.FO
 import ProofLab.Descriptive.FOSentence
 import ProofLab.Descriptive.ESO
 import ProofLab.Descriptive.LFP
+import ProofLab.Descriptive.Reachability
 import ProofLab.Descriptive.LFPMeta
